7c1d8882b167dc1ea441cac2e4211389ba916196
Bibek Chaudhary <bibek4929@gmail.com>
2026-03-06 14:09:15 +0545

    refactor(core): introduce generator context abstraction

---
+++ backend/GeneratorContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Generator.Core;
+
+public class GeneratorContext
+{
+    public GeneratorConfiguration Configuration { get; }
+
+    public string RootPath { get; }
+
+    public string SolutionPath => Path.Combine(RootPath, Configuration.ProjectName);
+
+    public GeneratorContext(GeneratorConfiguration configuration, string rootPath)
+    {
+        Configuration = configuration;
+        RootPath = rootPath;
+    }
+}
